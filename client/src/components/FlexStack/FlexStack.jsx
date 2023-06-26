2005bcc8caf86873ed533af8b27c4f8dacd41e02
webGhoul <84246173+web-ghoul@users.noreply.github.com>
2023-06-26 20:04:18 +0300

    Fourth Commit

--- client/src/components/FlexStack/FlexStack.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-import { Stack } from "@mui/material";
-
-export const FlexStack = styled(Stack)({
-    justifyContent:"center",
-    alignItems:"center",
-    flexDirection:"row",
-})
